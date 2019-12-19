7dfeafc28b37b1c76d8e16b9feb4f982fae40593
awpathum <awpathum@gmail.com>
2019-12-19 22:46:54 +0530

    crud functions added

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -1,79 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/note_detail.dart';
 
 class NoteList extends StatefulWidget {
-
-	@override
+  @override
   State<StatefulWidget> createState() {
-
     return NoteListState();
   }
 }
 
 class NoteListState extends State<NoteList> {
+  int count = 0;
 
-	int count = 0;
-
-	@override
+  @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-	    appBar: AppBar(
-		    title: Text('Notes'),
-	    ),
-
-	    body: getNoteListView(),
-
-	    floatingActionButton: FloatingActionButton(
-		    onPressed: () {
-		      debugPrint('FAB clicked');
-		    },
-
-		    tooltip: 'Add Note',
-
-		    child: Icon(Icons.add),
-
-	    ),
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB clicked');
+          navigateToDetail('Add Note');
+        },
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
+      ),
     );
   }
 
   ListView getNoteListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
 
-		TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text(
+              'Dummy Title',
+              style: titleStyle,
+            ),
+            subtitle: Text('Dummy Date'),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+              navigateToDetail('Edit Note');
+            },
+          ),
+        );
+      },
+    );
+  }
 
-		return ListView.builder(
-			itemCount: count,
-			itemBuilder: (BuildContext context, int position) {
-				return Card(
-					color: Colors.white,
-					elevation: 2.0,
-					child: ListTile(
-
-						leading: CircleAvatar(
-							backgroundColor: Colors.yellow,
-							child: Icon(Icons.keyboard_arrow_right),
-						),
-
-						title: Text('Dummy Title', style: titleStyle,),
-
-						subtitle: Text('Dummy Date'),
-
-						trailing: Icon(Icons.delete, color: Colors.grey,),
-
-						onTap: () {
-							debugPrint("ListTile Tapped");
-						},
-
-					),
-				);
-			},
-		);
+  void navigateToDetail(String title) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return NoteDetail(title);
+    }));
   }
 }
-
-
-
-
-
-
-
